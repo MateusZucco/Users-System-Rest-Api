e595a8a9bed5efed1ed64b03b8bbbae2ef3ba011
Mateus Zucco <zucco.developer@gmail.com>
2024-01-29 23:05:06 -0300

    feat(connection): return query errror

--- src/connection.js
+++ src/connection.js
@@ -1,49 +1,56 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
-module.exports = {
-  connect: async function () {
-    if (global.connection && global.connection.state !== "disconnected")
-      return global.connection;
+const connection = {
+    connect: async function () {
+        if (global.connection && global.connection.state !== 'disconnected') return global.connection;
 
-    const connection = await mysql.createConnection({
-      host: process.env.dbHost || "localhost",
-      port: process.env.dbPort || "3333",
-      user: process.env.dbUser || "root",
-      password: process.env.dbPass || "root",
-      database: process.env.dbName || "users_schema",
-    });
+        const connection = await mysql.createConnection({
+            host: process.env.dbHost || 'localhost',
+            port: process.env.dbPort || '3333',
+            user: process.env.dbUser || 'root',
+            password: process.env.dbPass || 'root',
+            database: process.env.dbName || 'users_schema',
+        });
 
-    await connection.connect((err) => {
-      if (err) throw err;
-      global.connection = connection;
-    });
+        await connection.connect((err) => {
+            if (err) throw err;
+            global.connection = connection;
+        });
 
-    return connection;
-  },
+        await connection.query('USE users_schema;');
 
-  query: async function (query) {
-    const conn = await this.connect();
-    const [rows] = await conn.query(query);
-    return rows;
-  },
+        return connection;
+    },
 
-  transaction: async function (queries) {
-    const conn = await this.connect();
-    try {
-      await conn.beginTransaction();
-      const queryPromises = [];
+    query: async function (query) {
+        try {
+            const conn = await connection.connect();
+            const [rows] = await conn.query(query);
+            return rows;
+        } catch (err) {
+            throw { error: err, message: err.message, success: false };
+        }
+    },
 
-      queries.forEach((query) => {
-        queryPromises.push(conn.query(query));
-      });
-      const results = await Promise.all(queryPromises);
-      await conn.commit();
-      await conn.end();
-      return results;
-    } catch (err) {
-      await connection.rollback();
-      await connection.end();
-      return Promise.reject(err);
-    }
-  },
+    transaction: async function (queries) {
+        const conn = await connection.connect();
+        try {
+            await conn.beginTransaction();
+            const queryPromises = [];
+
+            queries.forEach((query) => {
+                queryPromises.push(conn.query(query));
+            });
+            const results = await Promise.all(queryPromises);
+            await conn.commit();
+            await conn.end();
+            return results;
+        } catch (err) {
+            await connection.rollback();
+            await connection.end();
+            return Promise.reject(err);
+        }
+    },
 };
+
+module.exports = connection;
